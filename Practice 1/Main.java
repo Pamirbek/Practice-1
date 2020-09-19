c041a95ca34d9142fb76bfef073325b581a8550e
Pamirbek <32593658+Pamirbek@users.noreply.github.com>
2020-09-19 15:42:41 +0600

    Update Main.java

--- Practice 1/Main.java
+++ Practice 1/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ContactsManager myContactsManager = new ContactsManager();
-        myContactsManager.addContact(new Contact("Pamir", "+9967012222002"));
+        myContactsManager.addContact(new Contact("Pamir", "+996701333003"));
         myContactsManager.addContact(new Contact("Allen", "+996701404401"));
         myContactsManager.addContact(new Contact("Michael", "+996702060401"));
         myContactsManager.addContact(new Contact("Damian", "+996701909192"));
