e832178d851a1b410f4a738a3fe7da66e5bef0a5
Pamirbek <32593658+Pamirbek@users.noreply.github.com>
2020-09-19 14:43:41 +0600

    Add files via upload

---
+++ Practice 1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        ContactsManager myContactsManager = new ContactsManager();
+        myContactsManager.addContact(new Contact("Pamir", "+9967012222002"));
+        myContactsManager.addContact(new Contact("Allen", "+996701404401"));
+        myContactsManager.addContact(new Contact("Michael", "+996702060401"));
+        myContactsManager.addContact(new Contact("Damian", "+996701909192"));
+        myContactsManager.addContact(new Contact("Rondo", "+996775110132"));
+
+        myContactsManager.searchContact("Rondo");
+    }
+}
